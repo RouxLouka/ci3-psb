aa576e4d49070089ce4ba43240bd54ba0b34253f
Walid Algazali <walidalgazali14@gmail.com>
2021-10-18 10:13:46 +0700

    Create Dashboard bagian user
    Udah tampil view dashboard user

---
+++ application/views/user/dashboard.php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Walid Academy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url(); ?>assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/metisMenu.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slicknav.min.css">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/typography.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/default-css.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="<?php echo base_url(); ?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="index.html"><img src="<?php echo base_url(); ?>assets/images/icon/logo.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="<?php echo base_url('user/dashboard'); ?>"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                                
+                            </li>
+                            <li>
+                                <a href="<?php echo base_url('user/pendaftaran'); ?>"><i class="ti-layout-sidebar-left"></i><span>Pendaftaran</span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><i class="ti-user"></i><span>Log Out</span></a>
+                            
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+            <div class="header-area">
+                <div class="row align-items-center">
+                    <!-- nav and search button -->
+                    <div class="col-md-6 col-sm-8 clearfix">
+                        <div class="nav-btn pull-left">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <!-- profile info & task notification -->
+                </div>
+            </div>    
+            <!-- header area end -->
+            <!-- page title area start -->
+            <div class="page-title-area">
+                <div class="row align-items-center">
+                    <div class="col-md-6 col-sm-8">
+                        <div class="breadcrumbs-area clearfix">
+                            <h4 class="page-title pull-left">Dashboard</h4>
+                            <ul class="breadcrumbs pull-left">
+                                <li><a href="<?php echo base_url('user/dashboard'); ?>">>Home</a></li>
+                                <li><span>Dashboard</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- page title area end -->
+            
+                    <!-- testimonial area start -->
+                    
+                    <!-- testimonial area end -->
+            
+        <!-- main content area end -->
+        <div class="main-content-inner">
+                <div class="row mt-5 mb-5">
+                <div class="col-lg-12 mt-5">
+                <div class="card">
+                            <div class="card-body">
+                                <div class="d-sm-flex justify-content-between align-items-center">
+									<h2>Dashboard</h2>
+                                </div>
+                                <div class="market-status-table mt-4">
+                                    <div class="table-responsive">
+										 Selamat datang di sistem informasi Penerimaan Peserta Didik Baru (PPDB) Online.
+                                         <br><br>
+                                         Panduan Pendaftaran:
+                                         <br>1. Pada bagian menu, klik 'Pendaftaran'.
+                                         <br>2. Isi seluruh formulir yang ditampilkan kemudian periksa kembali, pastikan tidak ada data yang salah.
+                                         <br>3. Klik submit, kemudian klik Confirm. Setelah di-confirm, data tidak dapat diubah kembali.
+                                         <br>4. Jika sudah, bukti pendaftaran akan ditampilkan dan dapat diunduh menjadi PDF
+                                         <br>
+                                         <br>*Note: Pihak sekolah baru akan menerima data Anda setelah Anda klik 'Confirm'
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer area start-->
+        <footer>
+            <div class="footer-area">
+                <p>© Copyright 2018. All right reserved. Template by <a href="https://colorlib.com/wp/">Colorlib</a>.</p>
+            </div>
+        </footer>
+        <!-- footer area end-->
+    </div>
+    <!-- page container area end -->
+    
+    <!-- offset area end -->
+    <!-- jquery latest version -->
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- start chart js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <!-- start amcharts -->
+    <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+    <script src="https://www.amcharts.com/lib/3/ammap.js"></script>
+    <script src="https://www.amcharts.com/lib/3/maps/js/worldLow.js"></script>
+    <script src="https://www.amcharts.com/lib/3/serial.js"></script>
+    <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
+    <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+    <!-- all line chart activation -->
+    <script src="<?php echo base_url(); ?>assets/js/line-chart.js"></script>
+    <!-- all pie chart -->
+    <script src="<?php echo base_url(); ?>assets/js/pie-chart.js"></script>
+    <!-- all bar chart -->
+    <script src="<?php echo base_url(); ?>assets/js/bar-chart.js"></script>
+    <!-- all map chart -->
+    <script src="<?php echo base_url(); ?>assets/js/maps.js"></script>
+    <!-- others plugins -->
+    <script src="<?php echo base_url(); ?>assets/js/plugins.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+</body>
+
+</html>
