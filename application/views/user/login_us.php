94e861ccfd1aac2d4e33474e9d0b8a8bba774948
Walid Algazali <walidalgazali14@gmail.com>
2021-10-25 15:13:20 +0700

    Update View Login User
    Mungkin udah ok(?)

--- application/views/user/login_us.php
+++ application/views/user/login_us.php
@@ -37,25 +37,44 @@
     <div class="login-area">
         <div class="container">
             <div class="login-box ptb--100">
-                <form>
+                <form method="post" action="<?php echo base_url(); ?>login_us_proses">
                     <div class="login-form-head">
                         <h4>Sign In</h4>
-                        <p>Hello there, Sign in and start managing your Admin Template</p>
+                        <p>Masukkan Username dan Password Anda</p>
                     </div>
+                    <div class="card-body">
+				    <?php 
+				    if($this->session->flashdata('error') !='')
+				    {
+					    echo '<div class="alert alert-danger" role="alert">';
+					    echo $this->session->flashdata('error');
+					    echo '</div>';
+				    }
+				    ?>
+ 
+				    <?php 
+				    if($this->session->flashdata('success_register') !='')
+				    {
+					    echo '<div class="alert alert-info" role="alert">';
+					    echo $this->session->flashdata('success_register');
+					    echo '</div>';
+				    }
+				    ?>
+
                     <div class="login-form-body">
                         <div class="form-gp">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" id="exampleInputEmail1">
-                            <i class="ti-email"></i>
+                            <label for="username_us">Username</label>
+                            <input type="text" name="username_us" id="username_us">
+                            <i class="ti-user"></i>
                             <div class="text-danger"></div>
                         </div>
                         <div class="form-gp">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" id="exampleInputPassword1">
+                            <label for="password_us">Password</label>
+                            <input type="password" name="password_us" id="password_us">
                             <i class="ti-lock"></i>
                             <div class="text-danger"></div>
                         </div>
-                        <div class="row mb-4 rmber-area">
+                        <!--<div class="row mb-4 rmber-area">
                             <div class="col-6">
                                 <div class="custom-control custom-checkbox mr-sm-2">
                                     <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
@@ -65,20 +84,20 @@
                             <div class="col-6 text-right">
                                 <a href="#">Forgot Password?</a>
                             </div>
-                        </div>
+                        </div>-->
                         <div class="submit-btn-area">
                             <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
-                            <div class="login-other row mt-4">
+                            <!--<div class="login-other row mt-4">
                                 <div class="col-6">
                                     <a class="fb-login" href="#">Log in with <i class="fa fa-facebook"></i></a>
                                 </div>
                                 <div class="col-6">
                                     <a class="google-login" href="#">Log in with <i class="fa fa-google"></i></a>
                                 </div>
-                            </div>
+                            </div>-->
                         </div>
                         <div class="form-footer text-center mt-5">
-                            <p class="text-muted">Don't have an account? <a href="register.html">Sign up</a></p>
+                            <p class="text-muted">Belum mempunyai akun? <a href="<?php echo base_url(); ?>register_us">Sign up</a></p>
                         </div>
                     </div>
                 </form>
