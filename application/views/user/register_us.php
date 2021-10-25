1ce9869fc981cec4c3a027f9f3d247356e103910
Walid Algazali <walidalgazali14@gmail.com>
2021-10-25 15:13:02 +0700

    Create View Register
    Udah ok

---
+++ application/views/user/register_us.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Sign up - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url(); ?>assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/themify-icons.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/metisMenu.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slicknav.min.css">
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+    <!-- others css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/typography.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/default-css.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="<?php echo base_url(); ?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- login area start -->
+    <div class="login-area">
+        <div class="container">
+            <div class="login-box ptb--100">
+            <form method="post" action="<?php echo base_url(); ?>register_proses">
+                    <div class="login-form-head">
+                        <h4>Sign up</h4>
+                        <p>Silahkan masukkan data untuk daftar sebagai user</p>
+                    </div>
+                    <div class="card-body">
+				    <?php 
+				    if($this->session->flashdata('error') !='')
+				    {
+					    echo '<div class="alert alert-danger" role="alert">';
+					    echo $this->session->flashdata('error');
+					    echo '</div>';
+				    }
+				    ?>
+                    
+                    <div class="login-form-body">
+                        <div class="form-gp">
+                            <label>Username</label>
+                            <input type="text" name="username_us" id="username_us">
+                            <i class="ti-user"></i>
+                            <div class="text-danger"></div>
+                        </div>
+                        <div class="form-gp">
+                            <label>Email</label>
+                            <input type="email" name="email_us" id="email_us">
+                            <i class="ti-email"></i>
+                            <div class="text-danger"></div>
+                        </div>
+                        <div class="form-gp">
+                            <label>Password</label>
+                            <input type="password" name="password_us" id="password_us">
+                            <i class="ti-lock"></i>
+                            <div class="text-danger"></div>
+                        </div>
+                        <!--<div class="form-gp">
+                            <label for="exampleInputPassword2">Confirm Password</label>
+                            <input type="password" id="exampleInputPassword2">
+                            <i class="ti-lock"></i>
+                            <div class="text-danger"></div>
+                        </div>-->
+                        <div class="submit-btn-area">
+                            <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
+                        </div>
+                        <div class="form-footer text-center mt-5">
+                            <p class="text-muted">Sudah mempunyai akun? <a href="<?php echo base_url(); ?>login_us">Sign in</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- login area end -->
+
+    <!-- jquery latest version -->
+    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.slicknav.min.js"></script>
+    
+    <!-- others plugins -->
+    <script src="<?php echo base_url(); ?>assets/js/plugins.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+</body>
+
+</html>
